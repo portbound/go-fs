771c846cac227c895d288497b5b6f4407191e71b
portbound <prtbnd@proton.me>
2025-07-18 23:22:06 -0400

    REFACTORING file_service.go

--- internal/services/file_service.go
+++ internal/services/file_service.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/portbound/go-fs/internal/models"
@@ -21,59 +22,44 @@ func NewFileService(repo repositories.FileRepository, localStoragePath string) *
 }
 
 func (fs *FileService) UploadFile(r io.Reader, metadata *models.FileMetadata) error {
+	metadata.ID = uuid.New()
 
-
-	outFile, err := os.Create(tmpPath)
+	_, bytes, err := streamToDisk(r, metadata, fs.localStoragePath)
 	if err != nil {
-		return fmt.Errorf("failed to create tmp file: %w", err)
-	}
-	defer outFile.Close()
-
-	bytes, err := io.Copy(outFile, r)
-	if err != nil {
-		os.Remove(tmpPath)
-		return fmt.Errorf("failed to write to tmp file: %w", err)
+		return err
 	}
 
+	// TODO: implement streamToCloud()
+	// Streaming to the disk from the client first
+	// We don't want to funnel from client => bucket directly since that will keep the client waiting longer than they need to
+	// We can stream from /tmpdir => bucket instead, and after successful upload to cloud delete the asset from /tmpdir
+
+	metadata.UploadDate = time.Now()
 	metadata.Size = bytes
 	return nil
-	// _, err := streamToDisk(r, metadata)
-	// if err != nil {
-	// 	return err
-	// }
-	// return nil
 }
 
-func uploadToCloud(tmpPath string, metadata *models.FileMetadata) {
-	panic("unimplemented")
-}
+func streamToDisk(r io.Reader, metadata *models.FileMetadata, localStoragePath string) (string, int64, error) {
+	tmpDir := filepath.Join(localStoragePath, "tmp")
 
-func streamToDisk(r io.Reader, metadata *models.FileMetadata) (string, error) {
-	cwd, err := os.Getwd()
+	if err := os.MkdirAll(tmpDir, 0755); err != nil {
+		return "", 0, fmt.Errorf("failed to create tmp storage dir: %w", err)
+	}
+
+	tmpFileName := fmt.Sprintf("%s-%s", metadata.ID.String(), metadata.Name)
+	tmpFilePath := filepath.Join(tmpDir, tmpFileName)
+
+	tmpFile, err := os.Create(tmpFilePath)
 	if err != nil {
-		return "", fmt.Errorf("failed to get cwd: %w", err)
+		return "", 0, fmt.Errorf("failed to create tmp file: %w", err)
 	}
+	defer tmpFile.Close()
 
-	tmpStorage := filepath.Join(cwd, "tmp")
-	if err := os.MkdirAll(tmpStorage, 0755); err != nil {
-		return "", fmt.Errorf("failed to create tmp storage dir: %w", err)
-	}
-
-	filename := fmt.Sprintf("%s-%s", uuid.New().String(), metadata.Name)
-	tmpPath := filepath.Join(tmpStorage, filename)
-
-	outFile, err := os.Create(tmpPath)
+	bytes, err := io.Copy(tmpFile, r)
 	if err != nil {
-		return "", fmt.Errorf("failed to create tmp file: %w", err)
-	}
-	defer outFile.Close()
-
-	bytes, err := io.Copy(outFile, r)
-	if err != nil {
-		os.Remove(tmpPath)
-		return "", fmt.Errorf("failed to write to tmp file: %w", err)
+		os.Remove(tmpFilePath)
+		return "", 0, fmt.Errorf("failed to write to tmp file: %w", err)
 	}
 
-	metadata.Size = bytes
-	return tmpPath, nil
+	return tmpFilePath, bytes, nil
 }
