e18fcd02d1d3b51ca3c7474e44d1cc1057be3898
portbound <token.api.guy@gmail.com>
2025-08-25 20:33:32 -0400

    refactoring

--- internal/utils/ioutil.go
+++ internal/utils/ioutil.go
@@ -9,32 +9,37 @@ import (
 	"path/filepath"
 )
 
-func StageFileToDisk(ctx context.Context, path string, fileName string, reader io.Reader) (string, error) {
+type chanl struct {
+	bytesWritten int64
+	err          error
+}
+
+func StageFileToDisk(ctx context.Context, path string, fileName string, reader io.Reader) (string, int64, error) {
 	if err := os.MkdirAll(path, 0755); err != nil {
-		return "", fmt.Errorf("util.StageFileToDisk: failed to create storage dir at '%s': %w", path, err)
+		return "", 0, fmt.Errorf("util.StageFileToDisk: failed to create storage dir at '%s': %w", path, err)
 	}
 
 	file, err := os.Create(filepath.Join(path, fileName))
 	if err != nil {
-		return "", fmt.Errorf("util.StageFileToDisk: failed to create temp file: %w", err)
+		return "", 0, fmt.Errorf("util.StageFileToDisk: failed to create temp file: %w", err)
 	}
 	defer file.Close()
 
-	ch := make(chan error, 1)
+	ch := make(chan *chanl, 1)
 	go func() {
-		_, copyErr := io.Copy(file, reader)
-		ch <- copyErr
+		bytesWritten, copyErr := io.Copy(file, reader)
+		ch <- &chanl{bytesWritten: bytesWritten, err: copyErr}
 	}()
 
 	select {
 	case <-ctx.Done():
 		os.Remove(file.Name())
-		return "", ctx.Err()
-	case chanErr := <-ch:
-		if chanErr != nil {
+		return "", 0, ctx.Err()
+	case result := <-ch:
+		if result.err != nil {
 			os.Remove(file.Name())
-			return "", fmt.Errorf("util.StageFileToDisk: failed to write to tmp file: %w", chanErr)
+			return "", 0, fmt.Errorf("util.StageFileToDisk: failed to write to tmp file: %w", result.err)
 		}
-		return file.Name(), nil
+		return file.Name(), result.bytesWritten, nil
 	}
 }
